40368795e00285811ff8772d07507dea6cf762a3
I68439663 <abajin@myseneca.ca>
2023-04-15 11:11:47 -0400

    update: cors

--- src/server.js
+++ src/server.js
@@ -9,6 +9,25 @@ require('dotenv').config();
 // express app instance
 const app = require('./app');
 
+// cors
+const cors = require('cors');
+
+const allowlist = [
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+  'https://silver-sundae-d1d08d.netlify.app',
+];
+
+const corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
 // get port number
 const port = parseInt(process.env.PORT || 8080, 10);
 
